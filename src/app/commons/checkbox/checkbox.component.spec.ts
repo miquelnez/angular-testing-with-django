aa2ab3ac157b49ae53d5a2600a7fd5e770bc26a5
Miquelnez <miquelnez@gmail.com>
2019-10-02 18:36:01 +0200

    checkbox tested

--- src/app/commons/checkbox/checkbox.component.spec.ts
+++ src/app/commons/checkbox/checkbox.component.spec.ts
@@ -1,25 +1,39 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CheckboxComponent } from './checkbox.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('CheckboxComponent', () => {
   let component: CheckboxComponent;
   let fixture: ComponentFixture<CheckboxComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CheckboxComponent ]
-    })
-    .compileComponents();
-  }));
+  let element: HTMLElement;
+  let elementCheckbox: HTMLElement;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CheckboxComponent ],
+      // schemas: [NO_ERRORS_SCHEMA],
+    })
+    .compileComponents();
     fixture = TestBed.createComponent(CheckboxComponent);
     component = fixture.componentInstance;
+    fixture.detectChanges();
+    element = fixture.debugElement.nativeElement;
+    console.log(element);
+    elementCheckbox = element.querySelector('input');
+    console.log(elementCheckbox);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('click on a checkbox should emit a changed event', () => {
+    spyOn(component.changed, 'emit');
+    elementCheckbox.dispatchEvent(new Event('change'));
+    fixture.detectChanges();
+    expect(component.changed.emit).toHaveBeenCalled();
+  });
+
 });
--- src/app/commons/checkbox/checkbox.component.ts
+++ src/app/commons/checkbox/checkbox.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-checkbox',
   templateUrl: './checkbox.component.html',
   styleUrls: ['./checkbox.component.scss']
 })
-export class CheckboxComponent implements OnInit {
+export class CheckboxComponent {
   @Input()
   checked: boolean;
+
+  @Output()
+  changed: EventEmitter<any>;
+
   constructor() {
     this.checked = true;
+    this.changed = new EventEmitter();
+
   }
 
-  ngOnInit() {
+  handleChange(ev) {
+    ev.stopPropagation();
+    this.changed.emit({ chedled: ev.target.checked });
   }
 
 }
