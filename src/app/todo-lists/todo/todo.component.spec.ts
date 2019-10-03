12380aa90c6ba779af12f6e3bdc30bb82fcf56b1
Miquelnez <miquelnez@gmail.com>
2019-10-03 17:47:14 +0200

    all tests in green

--- src/app/todo-lists/todo/todo.component.spec.ts
+++ src/app/todo-lists/todo/todo.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodoComponent } from './todo.component';
+import { CommonsModule } from 'src/app/commons/commons.module';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('TodoComponent', () => {
   let component: TodoComponent;
@@ -8,7 +10,9 @@ describe('TodoComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TodoComponent ]
+      imports: [ CommonsModule ],
+      declarations: [ TodoComponent ],
+      // schemas: [NO_ERRORS_SCHEMA],
     })
     .compileComponents();
   }));
