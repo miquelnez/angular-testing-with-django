ec4a36960496bfc9c3bc7c3e7e8f061166689c4b
Miquelnez <miquelnez@gmail.com>
2019-10-06 15:28:19 +0200

    TodoList component generated

--- src/app/todo-lists/todo-lists.module.ts
+++ src/app/todo-lists/todo-lists.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CommonsModule } from '../commons/commons.module';
 import { TodoComponent } from './todo/todo.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 
 
 @NgModule({
-  declarations: [TodoComponent],
+  declarations: [TodoComponent, TodoListComponent],
   imports: [
     CommonModule,
     CommonsModule,
